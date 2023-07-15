e824dc5249e678619c189ffe58cec5c17233efa1
Dilshad-Noushad <dilshad.workspace@gmail.com>
2023-07-15 23:04:35 +0530

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Dilshad N on 31/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
